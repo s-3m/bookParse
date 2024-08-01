6a0b94905acd3beac90182dfab2bdf774c4b2ea2
Roman <s-3m@yandex.ru>
2024-08-01 12:49:18 +0300

    add create_task func

--- moscow/just_compare.py
+++ moscow/just_compare.py
@@ -20,44 +20,43 @@ headers = {
 
 to_del = []
 excel.ExcelFormatter.header_style = None
+count = 1
 
 
-async def to_check_item(article_list, past_day_result):
-    count = 1
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), trust_env=True) as session:
-        for article in article_list:
-            try:
-                link = f'{BASE_URL}/book/{article[:-2]}'
-                response = await session.get(link, headers=headers)
-                response_text = await response.text()
-                soup = bs(response_text, 'lxml')
-                age_control = soup.find('input', id='age_verification_form_mode')
-                script_index = 1
-                if age_control:
-                    closed_page = get_book_data(link)
-                    soup = bs(closed_page, "lxml")
-                    script_index = 5
+async def to_check_item(article, session, past_day_result):
+    global count
+    link = f'{BASE_URL}/book/{article[:-2]}'
+    await asyncio.sleep(10)
+    try:
+        response = await session.get(link, headers=headers)
+        response_text = await response.text()
+        soup = bs(response_text, 'lxml')
+        age_control = soup.find('input', id='age_verification_form_mode')
+        script_index = 1
+        if age_control:
+            closed_page = get_book_data(link)
+            soup = bs(closed_page, "lxml")
+            script_index = 5
 
-                if not soup.find('div', class_='book__buy'):
-                    del past_day_result[article]
-                    to_del.append(article)
-                else:
-                    need_element = soup.find_all('script')
-                    a = need_element[script_index].text.split('MbPageInfo = ')[1].replace('false', 'False').replace(
-                        'true', 'True')
-                    need_data_dict = eval(a[:-1])['Products'][0]
-                    stock = need_data_dict['Stock']
-                    past_day_result[article] = {'Наличие': stock}
+        if not soup.find('div', class_='book__buy'):
+            del past_day_result[article]
+            to_del.append(article)
+        else:
+            need_element = soup.find_all('script')
+            a = need_element[script_index].text.split('MbPageInfo = ')[1].replace('false', 'False').replace(
+                'true', 'True')
+            need_data_dict = eval(a[:-1])['Products'][0]
+            stock = need_data_dict['Stock']
+            past_day_result[article] = {'Наличие': stock}
 
-                print(f'\r{count}')
-                count = count + 1
-            except Exception as e:
-                with open('just_compare_error.txt', 'a+') as f:
-                    f.write(f'{article} ------ {e}\n')
-                continue
+        print(f'\r{count}')
+        count += 1
+    except Exception as e:
+        with open('just_compare_error.txt', 'a+') as f:
+            f.write(f'{link} ------ {e}\n')
 
 
-async def reparse_error(past_day_result):
+async def reparse_error(session, past_day_result):
     reparse_count = 0
     error_file = 'just_compare_error.txt'
     try:
@@ -70,18 +69,23 @@ async def reparse_error(past_day_result):
                     article_list = [link.split('/')[-2] + '.0' for link in error_links_list]
                     os.remove(error_file)
                 if error_links_list:
-                    await to_check_item(article_list, past_day_result)
+                    await to_check_item(article_list, session, past_day_result)
                 reparse_count += 1
     except:
         pass
 
 
 async def get_compare():
+    tasks = []
     past_day_result = pd.read_excel('new_stock.xlsx', converters={'Артикул': str}).set_index('Артикул').to_dict(
         'index')
     article_list = list(past_day_result.keys())
-    await to_check_item(article_list, past_day_result)
-    await reparse_error(past_day_result)
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), trust_env=True) as session:
+        for article in article_list:
+            task = asyncio.create_task(to_check_item(article, session, past_day_result))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+        await reparse_error(session, past_day_result)
 
     df = pd.DataFrame().from_dict(past_day_result, 'index')
     df.index.name = 'Артикул'
