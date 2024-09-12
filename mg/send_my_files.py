ebebfcac08be76d1e3c0bcf0d9da53335ff8eb3e
roman <s-3m@yandex.ru>
2024-09-12 12:13:21 +0300

    delete t

--- mg/send_my_files.py
+++ mg/send_my_files.py
@@ -6,7 +6,7 @@ from aiogram.types.input_file import FSInputFile
 import os
 
 async def tg_send_files(chat_id, files: list[str], subject=None):
-    bot = Bot('7283003827:AAF0Ii9JO8uOcURHS89j51Y8TJICar7RXFs')
+    bot = Bot('')
     if subject:
         await bot.send_message(chat_id, subject)
     for file in files:
