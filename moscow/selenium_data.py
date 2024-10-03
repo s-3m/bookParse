b343adad39e57bc3dc1155a0fd255c931f84cf6b
roman <s-3m@yandex.ru>
2024-10-03 12:01:57 +0300

    del not used imports

--- moscow/selenium_data.py
+++ moscow/selenium_data.py
@@ -1,9 +1,6 @@
-import json
 import os
-import pprint
 import time
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
